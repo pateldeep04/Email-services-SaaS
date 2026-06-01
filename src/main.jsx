984fb81cc2423e093f10193b9a23da9a36d3c50d
pateldeep04 <pateldeep042007@gmail.com>
2026-06-01 16:25:29 +0530

    chore: silence React Router v7 future flag warnings

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
     <App />
   </BrowserRouter>
 );
